236631680d02ddb4f1411302c09d570c1f809edd
Alex Small <alexsmall409@gmail.com>
2025-09-04 00:54:50 +0100

    Add jigsaw segmentation for uploaded images

--- PuzzleAM/Components/Pages/PuzzleGame.razor.cs
+++ PuzzleAM/Components/Pages/PuzzleGame.razor.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.JSInterop;
 
 namespace PuzzleAM.Components.Pages;
 
 public partial class PuzzleGame : ComponentBase
 {
     private string? imageDataUrl;
+    [Inject] private IJSRuntime JS { get; set; } = default!;
 
     private async Task OnInputFileChange(InputFileChangeEventArgs e)
     {
@@ -14,6 +16,7 @@ public partial class PuzzleGame : ComponentBase
         using var ms = new MemoryStream();
         await stream.CopyToAsync(ms);        // ensures the full file is read
         imageDataUrl = $"data:{file.ContentType};base64,{Convert.ToBase64String(ms.ToArray())}";
+        await JS.InvokeVoidAsync("createPuzzle", imageDataUrl, "puzzleContainer");
     }
 }
 
